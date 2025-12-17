cee1635f66a963c9c5dee2545a91f5e7a4863b36
Mason-tom <mason.tom@stu.ocsb.ca>
2025-12-17 12:51:47 -0500

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,32 @@
 /* Copyright (c) 2020 MTHS All rights reserved
- *
- * Created by: Mason Tom
- * Created on: Sep 2025
- * This program ...
+*
+* Created by: Sami Osman
+* Created on: Nov 2025
+* This program using a radio feature
 */
 
-basic.showString('Hello, World!')
+// variables
+let distanceOfObstacle: number = 0
+
+// cleanse
+radio.setGroup(39)
+basic.clearScreen()
+basic.showIcon(IconNames.Giraffe)
+
+// grabs distance
+while (true) {
+    distanceOfObstacle = sonar.ping(
+        DigitalPin.P12,
+        DigitalPin.P13,
+        PingUnit.Centimeters
+    )
+    if (distanceOfObstacle < 10) {
+        radio.sendString('Too Close bub')
+    }
+    radio.onReceivedString(function (receivedString: string) {
+        basic.clearScreen()
+        basic.showString(receivedString)
+        basic.showIcon(IconNames.Happy)
+    })
+}
+
